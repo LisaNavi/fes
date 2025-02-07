7af2016134959f8864f9ebc0a95e61bae1a3cfc3
fu_ca <shion.b2008@gmail.com>
2025-02-08 01:58:20 +0900

    経路案内（一階のみ）を追加
    遂に完成しました。
    α版です。

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let dis_number = document.getElementById("floorid");
 let panel = document.getElementById("information");
 let inputbox = document.getElementById("input");
 let schedulebox = document.getElementById("cls-table");
+let inputS = document.getElementById("input_start");
+let inputG = document.getElementById("input_goal");
 
 // クッキーから読み込み
 let userdata = {};
@@ -56,6 +58,13 @@ function search(num){
                 document.getElementById("info-btn").style.display = "none";
         }
     }
+}
+
+function open_route()
+{
+    var route_pane = document.getElementById("route_panel");
+    route_pane.style.visibility = "visible";
+    
 }
 
 // URLボタン用
@@ -113,3 +122,59 @@ window.onresize = function() {
 }
 
 panel.addEventListener("click", function() {panel.style.visibility = "hidden";});
+
+
+//経路検索
+
+function search_route(Snum, Gnum) {
+    if (Number.isInteger(Snum)){
+        alert("部屋番号を入力してください。");
+    }
+    else if (!(Snum > 100 && Snum < 1000)) {
+        alert("3桁である必要があります。");
+    }
+    else if (!(Snum[0] < 5 && Snum[0] > 0))
+    {
+        alert(Snum[0] + "階は存在しません。");
+    }
+    else {
+        if (typeof data[Snum] == "undefined"){
+            alert("指定された部屋番号は登録されていません。");
+        }
+        else {
+            var Sx = path[Snum][0];
+            var Sy = path[Snum][1];
+            map.contentWindow.room = Snum;
+            map.contentWindow.flchange(Snum-1);
+            map.contentWindow.showup(Snum);
+            dis_number.textContent = Snum[0] + "F";
+            dis_number.style.background=colors[Snum[0] - 1];
+        }
+    }
+    if (Number.isInteger(Gnum)){
+        alert("部屋番号を入力してください。");
+    }
+    else if (!(Gnum > 100 && Gnum < 1000)) {
+        alert("3桁である必要があります。");
+    }
+    else if (!(Gnum[0] < 5 && Gnum[0] > 0))
+    {
+        alert(Gnum[0] + "階は存在しません。");
+    }
+    else {
+        if (typeof data[Gnum] == "undefined"){
+            alert("指定された部屋番号は登録されていません。");
+        }
+        else {
+            var Gx = path[Gnum][0];
+            var Gy = path[Gnum][1];
+            map.contentWindow.room = Gnum;
+            map.contentWindow.flchange(Gnum-1);
+            map.contentWindow.showup(Gnum);
+            dis_number.textContent = Gnum[0] + "F";
+            dis_number.style.background=colors[Gnum[0] - 1];
+        }
+    }
+    map.contentWindow.draw(Sx,Sy,Gx,Gy);
+}
+
